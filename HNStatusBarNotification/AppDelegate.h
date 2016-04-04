4979e47081168708cb9a242e33099dad0698b92d
ZakariyyaSv <1248800415@qq.com>
2016-04-04 10:51:46 +0800

    Initial Commit

---
+++ HNStatusBarNotification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HNStatusBarNotification
+//
+//  Created by 许浩男 on 16/4/4.
+//  Copyright © 2016年 Zakariyya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HNStatusBarNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HNStatusBarNotification
+//
+//  Created by 许浩男 on 16/4/4.
+//  Copyright © 2016年 Zakariyya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
